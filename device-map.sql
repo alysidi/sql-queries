14ee59f88065cd1274a986f9cde42c701b726f7d
Aly <aly.sidi@generac.com>
2021-01-28 14:05:06 -0800

    device map

--- device-map.sql
+++ device-map.sql
@@ -1,11 +1,11 @@
 
 \timing
 -- individual inverters
-WITH DATA AS
+WITH data AS
     ( SELECT device_id, host_rcpn, st, device_type, timestamp_utc
      FROM status.device_shadow ds
       JOIN unnest(ARRAY[
-       '000100071818', '000100072021', '0001000712E6', '000100072E46', '0001000705DB', '000100070B1E'
+       '000100072DAB', '000100071818', '000100072021', '0001000712E6', '000100072E46', '0001000705DB', '000100070B1E'
         ]) host
      ON host_rcpn = host
      )
@@ -32,20 +32,18 @@ LEFT JOIN status.rcp_state r ON left(to_hex(d.st)::text, -1) || 0 = to_hex(r.sta
 LEFT JOIN status.ess_device_info e ON d.host_rcpn=e.host_rcpn
 AND d.device_id=e.device_id
 LEFT JOIN status.nameplate n ON d.device_id = n.device_id
--- latest row from device info
-WHERE (e.created_timestamp_utc=
-        (SELECT max(created_timestamp_utc)
-         FROM status.ess_device_info
-         WHERE device_id=d.device_id
-             AND host_rcpn=d.host_rcpn)
-      OR e.created_timestamp_utc IS NULL)
--- latest row from nameplate
-AND (n.created_timestamp_utc=
-        (SELECT max(created_timestamp_utc)
-         FROM status.nameplate
-         WHERE device_id=d.device_id)
-      OR n.created_timestamp_utc IS NULL)
 
+WHERE (e.created_timestamp_utc=
+           (SELECT max(created_timestamp_utc)
+            FROM status.ess_device_info
+            WHERE device_id=d.device_id
+                AND host_rcpn=d.host_rcpn)
+       OR e.created_timestamp_utc IS NULL) -- latest row from ess_device_info
+AND (n.created_timestamp_utc=
+         (SELECT max(created_timestamp_utc)
+          FROM status.nameplate
+          WHERE device_id=d.device_id)
+     OR n.created_timestamp_utc IS NULL)  -- latest row from nameplate
 
 
 
