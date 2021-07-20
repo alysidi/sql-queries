6a4c478c7d2ccf31170ec899f26e7214f26a3c95
Aly <aly.sidi@generac.com>
2021-07-20 10:38:17 -0700

    added 0 condition

--- alerts/7000.7FFFF.errors.resolve.sql
+++ alerts/7000.7FFFF.errors.resolve.sql
@@ -1,4 +1,4 @@
-SELECT parent.device_id, parent.host_rcpn, parent.device_type, json_agg(json_build_object('count', COALESCE(t.count,0))) as num_of_errors_in_window,
+SELECT parent.device_id, parent.host_rcpn, parent.device_type, t.count, json_agg(json_build_object('count', COALESCE(t.count,0))) as num_of_errors_in_window,
        'DEVICE_IN_ERROR_STATE' as alert_category,
         (NOW() - INTERVAL '12 hours') as transition_timestamp_utc
 --FROM (VALUES {} ) AS parent(device_id, host_rcpn) -- pass in tuples from chunks
@@ -7,11 +7,12 @@ LEFT JOIN
          ( SELECT device_id, count(device_id) as count
               FROM status.legacy_status_state_change lssc
               WHERE timestamp_utc >= NOW() - INTERVAL '12 hours'
-              AND st BETWEEN x'7000'::int AND x'7FFF'::int 
+              AND st BETWEEN x'7000'::int AND x'7FFF'::int
               GROUP BY lssc.device_id ) AS t
     ON t.device_id = parent.device_id
-GROUP BY parent.device_id, parent.host_rcpn, parent.device_type, t.count
---HAVING count(t.device_id) = 0;
+    AND t.count = 0
+GROUP BY parent.device_id, parent.host_rcpn, parent.device_type, t.count;
+
 
 
 
